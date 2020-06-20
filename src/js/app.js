95fcdc20a5352119d77684190d5f8116c5ac2f55
Tadii3108 <tadiwa.zingoni@umuzi.org>
2020-06-20 22:29:52 +0200

    added api key and dom elements

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,56 @@
+// Initial Values
+const API_KEY = '642a277b';
+const url = 'http://www.omdbapi.com/?apikey=642a277b'
 
 // Selecting elements from the DOM
-const buttonElement = document.querySelector('search');
-const inputElement = document.querySelector('input-value');
+const buttonElement = document.querySelector('#search');
+const inputElement = document.querySelector('#input-value');
+const movieSearchable = document.querySelector('#movies-searchable');
 
-buttonElement.onclick = function(event) {
-    event.preventDefault();
-    console.log('Hello World, this button has been clicked!');
+function movieSection(movies) {
+    return movies.map((movie) => {
+        return `
+            <img src=${movie.Poster} data-movie-id=${movie.imbdID}/>
+        `;
+    })
 }
+
+function createMovieContainer(movies) {
+    const movieElement = document.createElement('div');
+    movieElement.setAttribute('class', 'movie');
+
+    const movieTemplate = `
+    <section class="section">
+        ${ movieSection(movies) }
+    </section>
+    <div class="content">
+        <p class="content-close">X</p>
+    </div>
+    `;
+
+    movieElement.innerHTML = movieTemplate;
+    return movieElement
+}
+
+buttonElement.onclick = function(e) {
+    e.preventDefault();
+    const value = inputElement.value;
+
+    // Dynamically gets movie using title input by user
+    const newUrl = url + '&s=' + value;
+
+    // Fetching data from endpoint
+    fetch(newUrl)
+        .then((res) => res.json())
+        .then((data) => {
+            // data.results []
+            const movies = data.results;
+            const movieBlock = createMovieContainer(movies);
+            movieSearchable.appendChild(movieBlock);
+            console.log('Data: ', data);
+        })
+        .catch((error) => {
+            console.log('Error: ', error);
+        });
+    console.log('Value: ', value);
+};
